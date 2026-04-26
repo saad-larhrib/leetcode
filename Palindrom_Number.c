1e44f45d2ea35f1927bb0e37b7c94c29d1a4f24d
saad-larhrib <saad.larhrib06@gmail.com>
2026-04-26 21:21:11 +0100

    Optimal solution and Avoids overflow and Faster than the first version

--- Palindrom_Number.c
+++ Palindrom_Number.c
@@ -1,13 +1,12 @@
 bool isPalindrome(int x) {
-    long pal = 0;
-    int val = x;
     if (x < 0 || (x % 10 == 0 && x != 0)){
         return 0;
     }
     
+    int reversHalf = 0;
     while(x != 0){
-        pal =  pal * 10 + (x % 10);
+        reversHalf =  reversHalf * 10 + (x % 10);
         x = x / 10;
     }
-return pal == val;
+return (x == reversHalf) || (x == reversHalf/10);
 }
