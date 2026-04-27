3154a219c14a4c5dcc42d0ce8fe2b72fc5971557
saad-larhrib <saad.larhrib06@gmail.com>
2026-04-27 20:48:25 +0100

    Fixed the inifnite loop but the logic is correct

--- Lenght_of_LastWord.c
+++ Lenght_of_LastWord.c
@@ -1,17 +1,19 @@
 int lengthOfLastWord(char* s) {
     int i = 0;
-    // find length
-    while(s[i] != '\0')
-	    i++;
 
-    i--; // last character
-    
-    // skip trailing spaces
-    while(i >= 0 && s[i] == ' ')
-	    i--;
-    
+    // find end
+    while(s[i] != '\0') i++;
+    i--;
+
+    // skip spaces
+    while(i >= 0 && s[i] == ' ') i--;
+
     // count last word
-    int counter = 0;
-    while(i >= 0 && s[i] != ' ')
-	    counter++;
-return counter;}
+    int count = 0;
+    while(i >= 0 && s[i] != ' ') {
+        count++;
+        i--;
+    }
+
+    return count;
+}
