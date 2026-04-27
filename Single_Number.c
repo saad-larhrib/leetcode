6a213be19831272c1eeb33e802f10c32e7b7653c
saad-larhrib <saad.larhrib06@gmail.com>
2026-04-27 21:44:27 +0100

    Optimized solution and better logic

--- Single_Number.c
+++ Single_Number.c
@@ -1,25 +1,8 @@
 int singleNumber(int* nums, int numsSize) {
-    int table[numsSize];
-
+    int res = 0;
     for(int i = 0; i < numsSize; i++){
-        table[i] = 0;
+    	res ^= nums[numsSize];
     }
-
-    for(int i = 0; i < numsSize; i++){
-        for(int j = i + 1; j < numsSize; j++){
-            if(nums[i] == nums[j]){
-                table[i] = 1;
-                table[j] = 1;
-            }
-        }
-    }
-
-    for(int i = 0; i < numsSize; i++){
-        if(table[i] == 0){
-            return nums[i];
-        }
-    }
-
-    return -1;
+return res;
 }
 
